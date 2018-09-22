c951f2c37574de9e3d8366552e4e20fbdf46344b
Federico Giannini <f.giannini3@campus.unimib.it>
2018-09-22 14:45:42 +0200

    Ordinamento decrescente

--- Discover/src/main/java/it/unimib/discover/dao/impl/ListaDAO.java
+++ Discover/src/main/java/it/unimib/discover/dao/impl/ListaDAO.java
@@ -18,11 +18,11 @@ public class ListaDAO extends AbstractEntityDao<String, Lista> {
 				"from vw_liste_utente " +
 				"where USER_PROPRIETARIO=:user and ARCHIVIATA = 0 ";
 		if(ordineListe.equals("dataCreazione")) {
-			sql += "order by DATA_CREAZIONE";
+			sql += "order by DATA_CREAZIONE DESC";
 		} else if(ordineListe.equals("nome")) {
 			sql += "order by nome";
 		} else if(ordineListe.equals("numeroAttrazioni")) {
-			sql += "order by NUMERO_ATTRAZIONI";
+			sql += "order by NUMERO_ATTRAZIONI DESC";
 		} else {
 			sql += "order by NOME";
 		}
